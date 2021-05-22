60b4a5f4121cb10b41dad3dfa9e6c67fdbad5814
Kaira <kairita2016@gmail.com>
2021-05-22 20:26:20 -0300

    se agrego la validacion

--- validaciones.php
+++ validaciones.php
@@ -1,31 +1,32 @@
 <?php 
-  function ValidarCampo() {
-        if ($_POST['nombre'] === ""){
+  function ValidarCampos() {
+        if( empty($_POST['nombre'])){
             echo "Ingrese su nombre <br><br>";
         }
-        if ($_POST['apellido'] === ""){
+        if( empty($_POST['apellido'])){
             echo "Ingrese su apellido <br><br>";
         }
-        if ($_POST['telefono'] === ""){
+        if( empty($_POST['telefono'])){
             echo "Ingrese su número de teléfono <br><br>";
         }
-        if ($_POST['edad'] === ""){
+        if( empty($_POST['edad'])){
             echo "Ingrese su edad <br><br>";
         }
-        if ($_POST['fechadenacimiento'] === ""){
+        if( empty($_POST['fechadenacimiento'])){
             echo "Ingrese su fecha de naciemiento <br><br>";
         }
-        if ($_POST['email'] === ""){
+        if( empty($_POST['email'])){
             echo "Ingrese su email <br><br>";
         }
+        if(!is_numeric($_POST['edad'])){
+
+            echo "La edad debe ser ingresada en números <br><br>";
+        }
+        if(!is_numeric($_POST['telefono'])){
+            echo "El teléfono debe ser ingresado en números <br><br>";
+        }
   }
-  ValidarCampo();
+  ValidarCampos();
+ 
 
-  function ValidarNumero(){
-    
-
-
-
-
-
-  }
+  
