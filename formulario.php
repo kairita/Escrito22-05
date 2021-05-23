25715e59ac2ba3baa59ffd508d6e39ba9f7b108e
Kaira <kairita2016@gmail.com>
2021-05-22 21:00:59 -0300

    se conecto a la base de datos y se ingresaron los datos

--- formulario.php
+++ formulario.php
@@ -1,3 +1,22 @@
 <?php 
 
    require "validaciones.php";
+
+   function insertar($nombre,$apellido,$telefono,$edad,$fechadenacimiento,$email){
+       $conexion = new mysqli("127.0.0.1","root","","KAIRA");
+       $sql = "INSERT INTO persona(nombre,apellido,telefono,edad,fechadenacimiento,email) VALUES ('$nombre','$apellido','$telefono','$edad','$fechadenacimiento','$email')";
+       $conexion -> query($sql);
+       $conexion -> close();
+   }
+   
+   
+        insertar(
+           $_POST['nombre'],
+           $_POST['apellido'],
+           $_POST['telefono'],
+           $_POST['edad'],
+           $_POST['fechadenacimiento'],
+           $_POST['email']
+       );
+       echo "Ta pronto";
+   
